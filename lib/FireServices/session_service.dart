66fa1c9c410a67ea97033427b1df2e67aae0f3d7
JensIssa <71692396+JensIssa@users.noreply.github.com>
2023-05-12 12:34:58 +0200

    Send the collectionanmes

--- lib/FireServices/session_service.dart
+++ lib/FireServices/session_service.dart
@@ -5,6 +5,9 @@ import 'package:firebase_auth/firebase_auth.dart';
 
 import '../models/Session.dart';
 
+class CollectionNames {
+  static const sessions = 'sessions';
+}
 class SessionService {
   GameSession? _currentSession;
   final _currentSessionController = StreamController<GameSession>.broadcast();
@@ -16,16 +19,17 @@ class SessionService {
   Stream<GameSession> get currentSession => _currentSessionController.stream;
 
   Future<void> createSession(GameSession session) async {
-    final sessionRef = FirebaseFirestore.instance.collection('sessions').doc();
+    final sessionRef = FirebaseFirestore.instance.collection(CollectionNames.sessions).doc();
     session.id = sessionRef.id;
     await sessionRef.set(session.toMap());
   }
 
   Future<void> joinSession(String sessionId) async {
-    final sessionRef = FirebaseFirestore.instance.collection('sessions').doc(sessionId);
+    final sessionRef = FirebaseFirestore.instance.collection(CollectionNames.sessions).doc(sessionId);
     final session = await sessionRef.get();
     _currentSession = GameSession.fromMap(session.id, session.data()!);
     _currentSessionController.add(_currentSession!);
+
   }
 
   Future<void> leaveSession() async {
@@ -34,22 +38,22 @@ class SessionService {
   }
 
   Future<void> startSession() async {
-    final sessionRef = FirebaseFirestore.instance.collection('sessions').doc(_currentSession!.id);
+    final sessionRef = FirebaseFirestore.instance.collection(CollectionNames.sessions).doc(_currentSession!.id);
     await sessionRef.update({'currentQuestion': 0});
   }
 
   Future<void> nextQuestion() async {
-    final sessionRef = FirebaseFirestore.instance.collection('sessions').doc(_currentSession!.id);
+    final sessionRef = FirebaseFirestore.instance.collection(CollectionNames.sessions).doc(_currentSession!.id);
     await sessionRef.update({'currentQuestion': _currentSession!.currentQuestion + 1});
   }
 
   Future<void> endSession() async {
-    final sessionRef = FirebaseFirestore.instance.collection('sessions').doc(_currentSession!.id);
+    final sessionRef = FirebaseFirestore.instance.collection(CollectionNames.sessions).doc(_currentSession!.id);
     await sessionRef.update({'currentQuestion': _currentSession!.quiz.questions.length});
   }
 
   Future<void> answerQuestion(int answer) async {
-    final sessionRef = FirebaseFirestore.instance.collection('sessions').doc(_currentSession!.id);
+    final sessionRef = FirebaseFirestore.instance.collection(CollectionNames.sessions).doc(_currentSession!.id);
     final session = await sessionRef.get();
     final currentSession = GameSession.fromMap(session.id, session.data()!);
     final currentQuestion = currentSession.quiz.questions[currentSession.currentQuestion];
@@ -62,7 +66,7 @@ class SessionService {
   }
 
   Future<void> removePlayer(User user) async {
-    final sessionRef = FirebaseFirestore.instance.collection('sessions').doc(_currentSession!.id);
+    final sessionRef = FirebaseFirestore.instance.collection(CollectionNames.sessions).doc(_currentSession!.id);
     final session = await sessionRef.get();
     final currentSession = GameSession.fromMap(session.id, session.data()!);
     final players = currentSession.players;
