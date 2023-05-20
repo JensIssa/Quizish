bc01327e3ba9f9759beb479f88e4c84d96121d6e
JensIssa <71692396+JensIssa@users.noreply.github.com>
2023-05-20 03:56:15 +0200

    Work in progresss, trying to add a quiz to the session

--- lib/FireServices/RealTimeExample.dart
+++ lib/FireServices/RealTimeExample.dart
@@ -26,26 +26,29 @@ class GameSessionService {
       GameSession gameSession = GameSession(
         id: gameSessionId,
         host: null,
-        quiz: quiz,
         currentQuestion: 0,
         scores: null,
       );
       final sessionRef =
       _databaseReference.child('gameSessions').child(gameSessionId);
+      await addQuizToSesion(gameSessionId, quiz);
       await sessionRef.set(gameSession.toMap());
     } catch (e) {
       print('Error creating game session: $e');
     }
   }
+
   Future<void> addUserToSession(String sessionId, User? user) async {
     try {
-      final sessionRef = _databaseReference.child('gameSessions').child(sessionId);
+      final sessionRef = _databaseReference.child('gameSessions').child(
+          sessionId);
       DataSnapshot sessionSnapshot = await sessionRef.get();
       final dynamic sessionValue = sessionSnapshot.value;
 
       if (sessionValue != null && sessionValue is Map<dynamic, dynamic>) {
         final scores = sessionValue['scores'];
-        final updatedScores = scores != null ? Map.from(scores) : {}; // Initialize scores if null
+        final updatedScores = scores != null ? Map.from(scores) : {
+        }; // Initialize scores if null
         updatedScores[user?.uid] = 0;
         await sessionRef.child('scores').set(updatedScores);
       } else {
@@ -55,6 +58,44 @@ class GameSessionService {
       print('Error adding user to game session: $e');
     }
   }
+
+  Future<void> addQuizToSesion(String sessionId,  Quiz quiz) async {
+    try {
+      final sessionRef = _databaseReference.child('gameSessions').child(sessionId);
+      DataSnapshot sessionSnapshot = await sessionRef.get();
+      final dynamic sessionValue = sessionSnapshot.value;
+
+      if (sessionValue != null && sessionValue is Map<dynamic, dynamic>) {
+        final gameSessionMap = Map<String, dynamic>.from(sessionValue);
+        final quizMap = Map<String, dynamic>.from(gameSessionMap['quiz'] ?? {});
+        quizMap[quiz.id] = quiz.toMap();
+        gameSessionMap['quiz'] = quizMap;
+
+        await sessionRef.set(gameSessionMap);
+      } else {
+        print('Error: Invalid data or session does not exist');
+      }
+    } catch (e) {
+      print('Error adding quiz to game session: $e');
+    }
+  }
+
+
+
+
+
+
+  Future<void> updateCurrentQuestion(String sessionId, int currentQuestion) async {
+    try {
+      final sessionRef = _databaseReference.child('gameSessions').child(sessionId);
+      await sessionRef.child('currentQuestion').set(currentQuestion);
+    } catch (e) {
+      print('Error updating current question: $e');
+    }
+  }
+
+
+
 
   Future<void> removeUserFromSession(String sessionId, User user) async {
     try {
--- lib/models/Session.dart
+++ lib/models/Session.dart
@@ -12,13 +12,11 @@ class GameSession {
   GameSession(
       {required this.id,
       required this.host,
-      required this.quiz,
       required this.currentQuestion,
       required this.scores});
 
   GameSession.fromMap(Map<String, dynamic> data)
       :host = data['host'],
-        quiz = data['quiz'],
         currentQuestion = data['currentQuestion'],
         id = data['id'],
         scores = data['scores'];
